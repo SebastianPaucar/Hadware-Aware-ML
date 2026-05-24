3a148dd2ee8fea668cc0cbbb879dcc324045939e
SebastianPaucar <paucar.sebastian@hotmail.com>
2026-05-24 14:37:54 -0600

    full .def, sandbox and .sif

---
+++ set-up-sandbox.sh
@@ -0,0 +1,2 @@
+module load apptainer/1.4.1
+apptainer build --fakeroot --fix-perms -s alma9-haml alma9-haml.def
---
+++ set-up-sif.sh
@@ -0,0 +1,2 @@
+module load apptainer/1.4.1
+apptainer build --fakeroot --fix-perms alma9-haml.sif alma9-haml/
